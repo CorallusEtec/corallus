b46b6be2d3cefb545625371d9b4ef7d7d7111d08
Samu Mene <samuelsantiago2222@gmail.com>
2026-02-10 22:15:52 -0300

    Classes renomeadas

--- js/index.js
+++ js/index.js
@@ -26,13 +26,13 @@ const renderizarEquipe = () => {
 
         return `
             <div class="group h-100 w-64 perspective-[1000px]">
-                <div class="relative h-full w-full rounded-xl shadow-xl transition-all duration-700 transform-3d group-hover:[transform:rotateY(180deg)]">
+                <div class="relative h-full w-full rounded-xl shadow-xl transition-all duration-700 transform-3d group-hover:transform-[rotateY(180deg)]">
                     
-                    <div class="absolute inset-0 h-full w-full [backface-visibility:hidden] hover:z-0">
+                    <div class="absolute inset-0 h-full w-full backface-hidden hover:z-0">
                         <img class="h-full w-full rounded-xl object-cover shadow-lg" src="${membro.foto}" alt="${membro.nome}" />
                     </div>
 
-                    <div class="absolute inset-0 h-full w-full rounded-xl bg-white border border-gray-200 px-6 text-center text-gray-900 [backface-visibility:hidden] [transform:rotateY(180deg)] flex flex-col items-center justify-center z-10">
+                    <div class="absolute inset-0 h-full w-full rounded-xl bg-white border border-gray-200 px-6 text-center text-gray-900 backface-hidden transform-[rotateY(180deg)] flex flex-col items-center justify-center z-10">
                         <h5 class="text-xl font-bold mb-1">${membro.nome}</h5>
                         <p class="text-[11px] text-gray-500 italic mb-6 leading-tight">${membro.funcao}</p>
                         
