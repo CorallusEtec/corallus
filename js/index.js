eee5d5026d6d8f7d8b885c0c03abb26a6a50ed43
NkAndre <andre270708@gmail.com>
2026-02-27 22:09:59 -0300

    merge: atualizando branch com as novidades da main

--- js/index.js
+++ js/index.js
@@ -1,9 +1,15 @@
+
 import { renderAssuntos, renderizarEquipe, Carrosel } from './visual.js';
+
 const copy = document.querySelector("#copy");
 const selectEl = document.querySelector("#assunto");
 const dataAtual = new Date();
-copy.innerText = copy.innerText+" "+dataAtual.getFullYear();
 
+if (copy) {
+    copy.innerText = copy.innerText + " " + dataAtual.getFullYear();
+}
+
+// 2. Seleção de elementos do Carrossel
 const carroselBox = document.querySelector("#carroselBox");
 const btnProx = document.querySelector('#btnProx');
 const btnPrev = document.querySelector('#btnPrev');
@@ -12,29 +18,22 @@ const descProj = document.querySelector('#descProj');
 
 
 async function converterJson(arquivo) {
-    const data = await fetch(arquivo);
-    if(data.ok) {
-        const finalObj = await data.json();
-        return finalObj
+    const response = await fetch(arquivo);
+    if (response.ok) {
+        return await response.json();
     }
-    
+    return null;
 }
-/* jsonConvertido pega os dados do info.json e transforma em um objeto de fato,
- sendo possivel manipular no codigo*/
+
+
 const jsonConvertido = await converterJson('./js/info.json');
 
-/* Funções de renderização de componentes no HTML */
+if (jsonConvertido) {
+  
+    if (selectEl) {
+        renderAssuntos(selectEl, jsonConvertido.assuntos);
+    }
 
-/* USAR NO NOVO CARROSEL DE PROJETOS
-const carrosel = new Carrosel(carroselBox, jsonConvertido.projetos, descProj, progCarrosel, 0);
-btnProx.addEventListener('click', () =>carrosel.clickBtnProx());
-btnPrev.addEventListener('click', ()=>carrosel.clickBtnPrev());
-*/
-
-// USAR NO FORMULÁRIO DE FALE CONOSCO
-renderAssuntos(selectEl, jsonConvertido.assuntos);
-
-// USAR NOS CARDS DE EQUIPE
-/*renderizarEquipe(jsonConvertido.integrantes);*/
-
-
+    
+    renderizarEquipe(jsonConvertido.integrantes);
+}
